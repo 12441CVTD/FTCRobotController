093cce07364001aa6b760e948f7b5b2b2c1f1554
12441CVTD <145607783+12441CVTD@users.noreply.github.com>
2023-10-15 09:11:46 -0400

    Update snapshot4k.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/snapshot4k.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/snapshot4k.java
@@ -1,2 +1,11 @@
 //this is gonna mess everything up
 //camera stuff
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+//we need to look for percentages in color for picture and then we use that to do if then stuff to make the robot move
+telemetry("Connor is uncool");
