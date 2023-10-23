0265a06a8240161268ab5af4d8e9c871be3ae7b1
12441CVTD <145607783+12441CVTD@users.noreply.github.com>
2023-10-23 15:49:11 -0400

    Update snapshot4k.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/snapshot4k.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/snapshot4k.java
@@ -1,12 +1,3 @@
 //this is gonna mess everything up
 //camera stuff
 //help
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore
-
-@Autonomous(name="what am i doing", group="Robot")
-
-  public class RobotAutoDriveByTime_Linear extends Linear OpMode {
-int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-  }
