5b657c4bed16399459c1bbdf42da1045c356f9ac
12441CVTD <145607783+12441CVTD@users.noreply.github.com>
2023-10-25 15:42:20 -0400

    Update snapshot4k.java
    please dont use this the robot will probably start splitting atoms

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/snapshot4k.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/snapshot4k.java
@@ -149,4 +149,164 @@ For the Control Hub, the DS camera preview feature may prove very helpful for yo
 To use this feature, simply make sure that you've started a streaming session during the OpMode initialization. When running your OpMode, do **NOT** press start. Only press INIT. While the OpMode is in the INIT phase, you can open the overflow menu on the DriverStation and select the "Camera Stream" option. This provides a tap-to-refresh view of the pipeline output. Select `Camera Stream` again to close the preview and continue the OpMode.*/
 
 //ACTUAL CODE TIME
+//R
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/*
+ * This OpMode illustrates the concept of driving a path based on time.
+ * The code is structured as a LinearOpMode
+ *
+ * The code assumes that you do NOT have encoders on the wheels,
+ *   otherwise you would use: RobotAutoDriveByEncoder;
+ *
+ *   The desired path in this example is:
+ *   - Drive forward for 3 seconds
+ *   - Spin right for 1.3 seconds
+ *   - Drive Backward for 1 Second
+ *
+ *  The code is written in a simple form with no optimizations.
+ *  However, there are several ways that this type of sequence could be streamlined,
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="This is for idiot babies", group="Robot")
+
+public class RobotAutoDriveByTime_Linear extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    private DcMotor fL = null;
+    private DcMotor fR = null;
+    private DcMotor bL = null;
+    private DcMotor bR = null;
+    private ElapsedTime     runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+
+        // Initialize the drive system variables.
+        fL  = hardwareMap.get(DcMotor.class, "fL");
+        fR  = hardwareMap.get(DcMotor.class, "fR");
+        bL  = hardwareMap.get(DcMotor.class, "bL");
+        bR  = hardwareMap.get(DcMotor.class, "bR");
+int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "fungus", hardwareMap.appContext.getPackageName());
+        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
+        // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
+        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+        fL.setDirection(DcMotor.Direction.REVERSE);
+        fR.setDirection(DcMotor.Direction.FORWARD);
+        bL.setDirection(DcMotor.Direction.REVERSE);
+        bR.setDirection(DcMotor.Direction.FORWARD);
+        fL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+     OpenCvCamera fungus = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK);
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+       camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+{
+    @Override
+    public void onOpened()
+    {
+        // Usually this is where you'll want to start streaming from the camera (see section 4)
+    }
+    @Override
+    public void onError(int errorCode)
+    {
+       /*
+       * This will be called if the camera could not be opened
+       */
+    }
+});
+     camera.startsteaming(640, 480, OpenCvCameraRotation.UPRIGHT);
+     
+      fL.setPower(0);
+        fR.setPower(0);
+        bL.setPower(0);
+        bR.setPower(0);
+    }
+    public void driveWay(double lF, double rF, double lB, double rB, time s){
+        runtime.reset();
+        while(opModeIsActive() && (runtime.milliseconds() < s)){
+            fL.setPower(lF);
+            fR.setPower(rF);
+            bL.setPower(lB);
+            bR.setPower(rB);
+        }
+        fL.setPower(0);
+        fR.setPower(0);
+        bL.setPower(0);
+        bR.setPower(0);
+    }
+}
+/*package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+private DcMotor fl = null;
+private DcMotor fr = null;
+private DcMotor bl= null;
+private DcMotor br = null;
+
+ private ElapsedTime     runtime = new ElapsedTime();
+
+  @Override
+    public void runOpMode() {
+fl = hardwareMap.get(DcMotor.class "fl");
+fr = hardwareMap.get(DcMotor.class "fr")
+bl = hardwareMap.get(DcMotor.class "bl");
+br = hardwareMap.get(DcMotor.class "br");
+
+leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+ telemetry.addData("Status", "Ready to run");    
+        telemetry.update();
+
+}*/
