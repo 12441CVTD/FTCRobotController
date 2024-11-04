9d9103a91bbf6d63cfbdc0946f85e06506a501c4
robotics <12441@student.clearviewregional.edu>
2024-11-04 08:13:42 -0500

    ok

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Left_Encoderspls.java
@@ -0,0 +1,131 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+//you just have to adapt the stuff to 4 wheel and it might work maybe
+
+@Autonomous(name="badecnoder")
+
+public class Left_Encoderspls extends LinearOpMode {
+
+
+    public DcMotor motorLeft;
+    public DcMotor motorRight;
+
+
+
+    private ElapsedTime     runtime = new ElapsedTime();
+
+    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;
+    //this is wrong but idk what the right number is
+    static final double     WHEEL_DIAMETER_INCHES   = 1.7 ;
+    //this one is right
+    static final double     COUNTS_PER_INCH         = COUNTS_PER_MOTOR_REV /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    //it took me a while to realize this is pi
+
+    @Override
+
+
+    public void runOpMode() {
+
+        motorLeft = hardwareMap.dcMotor.get ("motorLeft");
+        motorRight = hardwareMap.dcMotor.get("motorRight");
+
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        motorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+//reset encoders during initialization
+        telemetry.addData("Status", "Resetting Encoders");
+        telemetry.update();
+
+        motorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        motorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        telemetry.addData("Path0",  "Starting at %7d :%7d",
+                motorLeft.getCurrentPosition(),
+                motorRight.getCurrentPosition());
+        telemetry.update();
+
+        waitForStart();
+
+//code motion here
+
+        encoderDrive(0.5,  12,  12, 5.0);
+
+    }
+
+
+
+
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches,
+                             double timeoutS) {
+        int newLeftTarget;
+        int newRightTarget;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newLeftTarget = motorLeft.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newRightTarget = motorRight.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            motorLeft.setTargetPosition(newLeftTarget);
+            motorRight.setTargetPosition(newRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            motorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            motorLeft.setPower(Math.abs(speed));
+            motorRight.setPower(Math.abs(speed));
+
+
+
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (motorLeft.isBusy() && motorRight.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
+                telemetry.addData("Path2",  "Running at %7d :%7d",
+                        motorLeft.getCurrentPosition(),
+                        motorRight.getCurrentPosition());
+                telemetry.update();
+            }
+
+
+
+            // Stop all motion;
+            motorLeft.setPower(0);
+            motorRight.setPower(0);
+
+            // Display complete for the driver.
+            telemetry.addData("Path", "Complete");
+            telemetry.update();
+
+
+            // Turn off RUN_TO_POSITION
+            motorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        }
+
+
+    }
+
+
+}
+
+
