cebff9844c79e8dabc0f1aff4264cab60a249e9c
robotics <12441@student.clearviewregional.edu>
2025-09-10 12:41:39 -0400

    nonsense

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/MecanumDrive.java
@@ -1,5 +1,24 @@
 package org.firstinspires.ftc.teamcode.Decode;
 
-public class MecanumDrive {
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class MecanumDrive {
+    private DcMotor fL = null;
+    private DcMotor bL = null;
+    private DcMotor fR = null;
+    private DcMotor bR = null;
+
+    public void runOpMode(){
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        fL  = hardwareMap.get(DcMotor.class, "fL");
+        fR = hardwareMap.get(DcMotor.class, "fR");
+        bL  = hardwareMap.get(DcMotor.class, "bL");
+        bR = hardwareMap.get(DcMotor.class, "bR");
+    }
 }
+
