3da61db2b0a241c315e06116bdaef18cf9d18415
robotics <12441@student.clearviewregional.edu>
2025-09-11 12:43:19 -0400

    nonsense

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/MecanumDrive.java
@@ -1,16 +1,30 @@
 package org.firstinspires.ftc.teamcode.Decode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class MecanumDrive {
+
+
+@TeleOp(name = "MecanumDrive NO USE")
+public class MecanumDrive extends LinearOpMode {
     private DcMotor fL = null;
     private DcMotor bL = null;
     private DcMotor fR = null;
     private DcMotor bR = null;
 
+    private Gamepad previousGP2 = new Gamepad();
+    private Gamepad currentGP2 = new Gamepad();
+
+    @Override
     public void runOpMode(){
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -19,6 +33,33 @@ public class MecanumDrive {
         fR = hardwareMap.get(DcMotor.class, "fR");
         bL  = hardwareMap.get(DcMotor.class, "bL");
         bR = hardwareMap.get(DcMotor.class, "bR");
+
+        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
+        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
+        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+        fL.setDirection(DcMotor.Direction.FORWARD);
+        fR.setDirection(DcMotor.Direction.FORWARD);
+        bL.setDirection(DcMotor.Direction.REVERSE);
+        bR.setDirection(DcMotor.Direction.REVERSE);
+
+        // Wait for the game to start (driver presses START)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Can be used to make toggleable inputs
+            previousGP2.copy(currentGP2);
+            currentGP2.copy(gamepad2);
+
+            // Format: if(currentGP2.*insertInput* && !previousGP2.*insertInput*)
+
+            // Setup a variable for each drive wheel to save power level for telemetry
+            double fLPower;
+            double bLPower;
+            double fRPower;
+            double bRPower;
+        }
     }
 }
 
