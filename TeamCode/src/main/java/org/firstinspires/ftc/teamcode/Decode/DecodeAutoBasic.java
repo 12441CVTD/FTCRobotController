5cdf182dae512480f7ee9eebead77327b72dad4c
robotics <12441@student.clearviewregional.edu>
2025-10-21 12:23:10 -0400

    get rid of pedro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/DecodeAutoBasic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/DecodeAutoBasic.java
@@ -1,29 +1,15 @@
 package org.firstinspires.ftc.teamcode.Decode;
 
-import org.firstinspires.ftc.teamcode.Decode.DecodeArms;
-import org.firstinspires.ftc.teamcode.Decode.DecodeIntake;
-import org.firstinspires.ftc.teamcode.Decode.DecodeMecanumDrive;
-
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import java.util.Timer;
-import java.util.TimerTask;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name = "Decode_Basic_Auto", group = "Decode")
-public class DecodeAutoBasic extends OpMode {
+@Autonomous
+public class DecodeAutoBasic extends LinearOpMode {
+
 
     @Override
-    public void init() {
-        DecodeArms launcher = new DecodeArms();
-        DecodeIntake intake = new DecodeIntake();
-        DecodeMecanumDrive chassis = new DecodeMecanumDrive();
+    public void runOpMode() throws InterruptedException {
 
-        Timer timer = new Timer();
-    }
-
-    @Override
-    public void loop() {
-        
     }
 }
