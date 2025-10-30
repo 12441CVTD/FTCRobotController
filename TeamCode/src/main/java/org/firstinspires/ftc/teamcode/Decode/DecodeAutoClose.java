e7b9a971c07ad53f199e5ae8f2f6e74d71fec8c0
robotics <12441@student.clearviewregional.edu>
2025-10-30 14:57:19 -0400

    Added file for close auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/DecodeAutoClose.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode.Decode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous
+public class DecodeAutoClose extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    DecodeArms launcher = null;
+    DecodeIntake intake = null;
+    DecodeMecanumDrive chassis = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        launcher = new DecodeArms(hardwareMap);
+        intake = new DecodeIntake(hardwareMap);
+        chassis = new DecodeMecanumDrive(hardwareMap);
+
+        runtime = new ElapsedTime();
+
+        waitForStart();
+
+        runtime.reset();
+
+        while(opModeIsActive() && runtime.milliseconds() < 1000){
+            chassis.drive(-0.3,0,0);
+        }
+
+        chassis.drive(0,0,0);
+
+        runtime.reset();
+
+        sleep(300);
+
+        while(opModeIsActive() && runtime.milliseconds() < 1000){
+            launcher.powAmplification();
+        }
+
+        runtime.reset();
+        sleep(300);
+
+        while(opModeIsActive() && runtime.milliseconds() < 1000){
+            launcher.gateOpen();
+        }
+
+        runtime.reset();
+        sleep(300);
+
+        while(opModeIsActive() && runtime.milliseconds() < 1000){
+            chassis.drive(-0.3, 0, 0);
+        }
+
+        chassis.drive(0,0,0);
+        launcher.gateClose();
+        launcher.powReversal();
+    }
+}
